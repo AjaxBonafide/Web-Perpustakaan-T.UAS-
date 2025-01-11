1e688845e06d752e4a1fbfbfad138b9626931c3f
irfan-pzv <irfanihsanramadhan@gmail.com>
2025-01-11 22:23:48 +0700

    Update homepage.php
    penambahan navbar

--- homepage.php
+++ homepage.php
@@ -10,10 +10,32 @@ if (!isset($_SESSION['isLoggedIn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="homepage.php">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Users</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tabel_buku.php">Buku</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Penulis</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
-    <a href="input.php">Tambah Data Buku</a>
     <a href="logout.php">Logout</a>
 </body>
 </html>
