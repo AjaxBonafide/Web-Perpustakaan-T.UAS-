5e69ddb4461c61a61d3470535843610f34aca564
irfan-pzv <irfanihsanramadhan@gmail.com>
2025-01-12 18:29:58 +0700

    Update input_data.php
    Menyesuaikan tabel_buku.php

--- input_data.php
+++ input_data.php
@@ -1,13 +1,23 @@
 <?php
+session_start();
 include "koneksi.php";
 
+if (!$_SESSION['isLoggedIn']) 
+    {
+        header("location: login.php");
+    }
+date_default_timezone_set('Asia/Jakarta');
 $judul = $_POST['judul'];
-$penulis = $_POST['penulis'];
+$tahun = $_POST['tahun'];
+$id_penulis = $_POST['id_penulis'];
+$nama_penulis = $_POST['nama_penulis'];
 
-$stmt = $koneksi->prepare("INSERT INTO buku (judul, penulis) VALUES (:judul, :penulis)");
-$stmt->bindParam(':judul', $judul);
-$stmt->bindParam(':penulis', $penulis);
-$stmt->execute();
+$dbh=$koneksi->prepare("INSERT INTO buku (judul, tahun, id_penulis, nama_penulis, created_by, created_at) VALUES (?,?,?,?,?,?)");
+$dbh->execute([$judul, $tahun, $id_penulis, $nama_penulis, $_SESSION['userid'], date("Y-m-d H:i:s")]);
+
+if (empty($judul) || empty($tahun) || empty($id_penulis) || empty($nama_penulis)) {
+    die("Semua data harus diisi.");
+}
 
 header("Location: homepage.php");
 ?>
